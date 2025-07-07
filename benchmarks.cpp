8742015a6f3ad34737f4ea67db4d8883acb2fa6e
omer.r <omer.r@campus.technion.ac.il>
2025-07-07 09:43:46 +0300

    Add swap2_bv and swap2_multi benchmarks

--- benchmarks.cpp
+++ benchmarks.cpp
@@ -33,6 +33,9 @@ check_result max_bv(unsigned int size) {
     params param(c);
     param.set("engine", "spacer");
     //set_param("verbose", 10);
+    param.set("fp.xform.slice", false);
+    param.set("fp.xform.inline_linear", false);
+    param.set("fp.xform.inline_eager", false);
     fp.set(param);
 
     // Variables
@@ -174,6 +177,9 @@ check_result opposite_signs_bv(unsigned int size) {
     // Set engine to Spacer explicitly (optional — it's the default)
     params param(c);
     param.set("engine", "spacer");
+    param.set("fp.xform.slice", false);
+    param.set("fp.xform.inline_linear", false);
+    param.set("fp.xform.inline_eager", false);
     //set_param("verbose", 10);
     fp.set(param);
 
@@ -308,6 +314,9 @@ check_result abs_bv(unsigned int size) {
     // Set engine to Spacer explicitly (optional — it's the default)
     params param(c);
     param.set("engine", "spacer");
+    param.set("fp.xform.slice", false);
+    param.set("fp.xform.inline_linear", false);
+    param.set("fp.xform.inline_eager", false);
     //set_param("verbose", 10);
     fp.set(param);
 
@@ -440,6 +449,9 @@ check_result cond_negate_bv(unsigned int size) {
     // Set engine to Spacer explicitly (optional — it's the default)
     params param(c);
     param.set("engine", "spacer");
+    param.set("fp.xform.slice", false);
+    param.set("fp.xform.inline_linear", false);
+    param.set("fp.xform.inline_eager", false);
     //set_param("verbose", 10);
     fp.set(param);
 
@@ -828,9 +840,216 @@ check_result swap_multi(unsigned int size) {      // bv - - -> int, unsigned var
     return result;
 }
 
+check_result swap2_bv(unsigned int size) {
+    context c;
+    fixedpoint fp(c);
+
+    // Declare sorts
+    sort bv_sort = c.bv_sort(size);
+    sort bool_sort = c.bool_sort();
+
+    // Declare relations
+    func_decl p = function("p", bv_sort, bv_sort, bv_sort, bv_sort, bool_sort);
+    func_decl q = function("q", bv_sort, bv_sort, bv_sort, bool_sort);
+    func_decl r0 = function("r0", bv_sort, bv_sort, bool_sort);
+    func_decl r1 = function("r1", bv_sort, bv_sort, bool_sort);
+    func_decl r2 = function("r2", bv_sort, bv_sort, bool_sort);
+    func_decl r3 = function("r3", bv_sort, bv_sort, bool_sort);
+
+    // Register them with the fixedpoint engine (required)
+    fp.register_relation(p);
+    fp.register_relation(q);
+    fp.register_relation(r0);
+    fp.register_relation(r1);
+    fp.register_relation(r2);
+    fp.register_relation(r3);
+
+    // Set engine to Spacer explicitly (optional — it's the default)
+    params param(c);
+    param.set("engine", "spacer");
+    param.set("fp.xform.slice", false);
+    param.set("fp.xform.inline_linear", false);
+    param.set("fp.xform.inline_eager", false);
+    //set_param("verbose", 10);
+    fp.set(param);
+
+    // Variables
+    expr x = c.bv_const("x", size);
+    expr y = c.bv_const("y", size);
+    expr a = c.bv_const("a", size);
+    expr b = c.bv_const("b", size);
+
+    // Rules
+    // ugt(x,y), ugt(y,0) --> p(x,y,0,0)
+    expr rule1 = forall(x, y, implies(ugt(x, y) && ugt(y, 0), p(x, y, c.bv_val(0, size), c.bv_val(0, size))));
+    symbol name1 = c.str_symbol("rule1");
+    fp.add_rule(rule1, name1);
+
+    // p(x,y,a,b), ult(b,y) --> p(x,y,a+1,b+1) 
+    expr rule2 = forall(x, y, a, b, implies(p(x, y, a, b) && ult(b, y), p(x, y, a + c.bv_val(1, size), b + c.bv_val(1, size))));
+    symbol name2 = c.str_symbol("rule2");
+    fp.add_rule(rule2, name2);
+
+    // p(x,y,a,b), !ult(b,y) --> q(x,a,b)
+    expr rule3 = forall(x, y, a, b, implies(p(x, y, a, b) && !ult(b, y), q(x, a, b)));
+    symbol name3 = c.str_symbol("rule3");
+    fp.add_rule(rule3, name3);
+
+    // q(x,a,b), ult(a,x) --> q(x,a+1,b)
+    expr rule4 = forall(x, a, b, implies(q(x, a, b) && ult(a, x), q(x, a + c.bv_val(1, size), b)));
+    symbol name4 = c.str_symbol("rule4");
+    fp.add_rule(rule4, name4);
+
+    // q(x,a,b), !ult(a,x) --> r0(a,b)
+    expr rule5 = forall(x, a, b, implies(q(x, a, b) && !ult(a, x), r0(a, b)));
+    symbol name5 = c.str_symbol("rule5");
+    fp.add_rule(rule5, name5);
+
+    // r0(a,b) --> r1(a^b,b)
+    expr rule6 = forall(a, b, implies(r0(a, b), r1(a ^ b, b)));
+    symbol name6 = c.str_symbol("rule6");
+    fp.add_rule(rule6, name6);
+
+    // r1(a,b) --> r2(a,b^a)
+    expr rule7 = forall(a, b, implies(r1(a, b), r2(a, b ^ a)));
+    symbol name7 = c.str_symbol("rule7");
+    fp.add_rule(rule7, name7);
+
+    // r2(a,b) --> r3(a^b,b)
+    expr rule8 = forall(a, b, implies(r2(a, b), r3(a ^ b, b)));
+    symbol name8 = c.str_symbol("rule8");
+    fp.add_rule(rule8, name8);
+
+    // r3(a,b) && !(ult(a,b)) --> false
+    expr query = exists(a, b, r3(a, b) && !ult(a, b));
+    check_result result = fp.query(query);
+
+    if (result == sat) {
+        std::cout << "SAT: Bad state is reachable.\n";
+    }
+    else if (result == unsat) {
+        std::cout << "UNSAT: Bad state is unreachable.\n";
+    }
+    else {
+        std::cout << "UNKNOWN.\n";
+    }
+
+    std::cout << "\nSpacer's output:\n";
+    std::cout << fp.get_answer() << "\n";
+
+    return result;
+}
+
+check_result swap2_multi(unsigned int size) { // int - - -> bv , unsigned variables
+    context c;
+
+    // Declare sorts
+    sort bv_sort = c.bv_sort(size);
+    sort int_sort = c.int_sort();
+    sort bool_sort = c.bool_sort();
+
+    MT_fixedpoint mtfp(c, /* is_signed */ false, size);
+
+    // Declare int relations
+    func_decl p_int = function("p_int", int_sort, int_sort, int_sort, int_sort, bool_sort);
+    func_decl q_int = function("q_int", int_sort, int_sort, int_sort, bool_sort);
+    func_decl r_int = function("r_int", int_sort, int_sort, bool_sort);
+
+    // Declare bv relations
+    func_decl r0_bv = function("r0_bv", bv_sort, bv_sort, bool_sort);
+    func_decl r1_bv = function("r1_bv", bv_sort, bv_sort, bool_sort);
+    func_decl r2_bv = function("r2_bv", bv_sort, bv_sort, bool_sort);
+    func_decl r3_bv = function("r3_bv", bv_sort, bv_sort, bool_sort);
+
+    // Register relations
+    mtfp.register_relation(p_int, Theory::IAUF);
+    mtfp.register_relation(q_int, Theory::IAUF);
+    mtfp.register_relation(r_int, Theory::IAUF);
+    mtfp.register_relation(r0_bv, Theory::BV);
+    mtfp.register_relation(r1_bv, Theory::BV);
+    mtfp.register_relation(r2_bv, Theory::BV);
+    mtfp.register_relation(r3_bv, Theory::BV);
+
+    // int variables
+    expr x_int = c.int_const("x_int");
+    expr y_int = c.int_const("y_int");
+    expr a_int = c.int_const("a_int");
+    expr b_int = c.int_const("b_int");
+
+    // bv variables
+    expr a_bv = c.bv_const("a_bv", size);
+    expr b_bv = c.bv_const("b_bv", size);
+
+    // int rules
+    // ugt(x,y), ugt(y,0) --> p(x,y,0,0)
+    expr rule1_int = forall(x_int, y_int, implies((x_int > y_int) && (y_int > 0) && bounds(c, x_int, false, size) && bounds(c, y_int, false, size), p_int(x_int, y_int, c.int_val(0), c.int_val(0))));
+    symbol name1 = c.str_symbol("rule1_int");
+    mtfp.add_rule(rule1_int, Theory::IAUF, name1);
+
+    // p(x,y,a,b), ult(b,y) --> p(x,y,a+1,b+1) 
+    expr rule2_int = forall(x_int, y_int, a_int, b_int, implies(p_int(x_int, y_int, a_int, b_int) && (b_int < y_int), p_int(x_int, y_int, a_int + c.int_val(1), b_int + c.int_val(1))));
+    symbol name2 = c.str_symbol("rule2_int");
+    mtfp.add_rule(rule2_int, Theory::IAUF, name2);
+
+    // p(x,y,a,b), !ult(b,y) --> q(x,a,b)
+    expr rule3_int = forall(x_int, y_int, a_int, b_int, implies(p_int(x_int, y_int, a_int, b_int) && !(b_int < y_int), q_int(x_int, a_int, b_int)));
+    symbol name3 = c.str_symbol("rule3_int");
+    mtfp.add_rule(rule3_int, Theory::IAUF, name3);
+
+    // q(x,a,b), ult(a,x) --> q(x,a+1,b)
+    expr rule4_int = forall(x_int, a_int, b_int, implies(q_int(x_int, a_int, b_int) && (a_int < x_int), q_int(x_int, a_int + c.int_val(1), b_int)));
+    symbol name4 = c.str_symbol("rule4_int");
+    mtfp.add_rule(rule4_int, Theory::IAUF, name4);
+
+    // q(x,a,b), !ult(a,x) --> r0(a,b)
+    expr rule5_int = forall(x_int, a_int, b_int, implies(q_int(x_int, a_int, b_int) && !(a_int < x_int), r_int(a_int, b_int)));
+    symbol name5 = c.str_symbol("rule5_int");
+    mtfp.add_rule(rule5_int, Theory::IAUF, name5);
+
+    // interface constraint int - - -> bv
+    // r_int(a,b) - - - -> r0(a',b')
+    mtfp.add_interface_constraint(r_int(a_int, b_int), Theory::IAUF, r0_bv(a_bv, b_bv), Theory::BV);
+
+    // r0(a,b) --> r1(a^b,b)
+    expr rule6_bv = forall(a_bv, b_bv, implies(r0_bv(a_bv, b_bv), r1_bv(a_bv ^ b_bv, b_bv)));
+    symbol name6 = c.str_symbol("rule6_bv");
+    mtfp.add_rule(rule6_bv, Theory::BV, name6);
+
+    // r1(a,b) --> r2(a,b^a)
+    expr rule7_bv = forall(a_bv, b_bv, implies(r1_bv(a_bv, b_bv), r2_bv(a_bv, b_bv ^ a_bv)));
+    symbol name7 = c.str_symbol("rule7_bv");
+    mtfp.add_rule(rule7_bv, Theory::BV, name7);
+
+    // r2(a,b) --> r3(a^b,b)
+    expr rule8_bv = forall(a_bv, b_bv, implies(r2_bv(a_bv, b_bv), r3_bv(a_bv ^ b_bv, b_bv)));
+    symbol name8 = c.str_symbol("rule8_bv");
+    mtfp.add_rule(rule8_bv, Theory::BV, name8);
+
+    // bv query
+    // r3(a,b) && !(ult(a,b)) --> false
+    expr_vector query_vars(c);
+    query_vars.push_back(a_bv);
+    query_vars.push_back(b_bv);
+    expr query_bv_pred = r3_bv(a_bv, b_bv);
+    expr query_bv_phi = !(ult(a_bv, b_bv));
+    check_result result = mtfp.query(query_vars, query_bv_pred, query_bv_phi, Theory::BV);
+
+    if (result == sat) {
+        std::cout << "SAT: Bad state is reachable.\n";
+    }
+    else if (result == unsat) {
+        std::cout << "UNSAT: Bad state is unreachable.\n";
+    }
+    else {
+        std::cout << "UNKNOWN.\n";
+    }
+
+    return result;
+}
+
 int main() {
     try {
-        unsigned int size = 4;
+        unsigned int size = 32;
         //max_bv(size);
         //max_multi(size);
         //opposite_signs_bv(size);
@@ -838,9 +1057,11 @@ int main() {
         //abs_bv(size);
         //abs_multi(size);
         //cond_negate_bv(size);
-        cond_negate_multi(size);
+        //cond_negate_multi(size);
         //swap_bv(size);
         //swap_multi(size);
+        //swap2_bv(size);
+        swap2_multi(size);
     }
     catch (exception& ex) {
         std::cout << "unexpected error: " << ex << "\n";
