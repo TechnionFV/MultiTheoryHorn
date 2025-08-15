ef6f0ae097a69f7f79c763c09af8af2281d9166d
Thomas Hashem <hashemthomas1@gmail.com>
2025-08-16 02:20:40 +0300

    [int2bv] Introduce Int2BvPreprocessor class
    This class, through the preprocess public method, changes
    the input expression: phi (a CNF formula) and creates:
    
    phi_2 = (phi_1 && !(psi_UNSAT)) || psi_SAT
    
    as described in the paper, which is then passed to the
    original translator.
    
    Currently, we successfully create SATOutOfBounds formulas.

---
+++ src/Int2BvPreprocessor.cpp
@@ -0,0 +1,226 @@
+#include "Int2BvPreprocessor.h"
+
+namespace multi_theory_horn {
+    Int2BvPreprocessor::Int2BvPreprocessor(z3::context& c, unsigned bv_size, bool is_signed) :
+        m_ctx(c),
+        m_bv_size(bv_size),
+        m_is_signed(is_signed),
+        m_vars_bound_lemmas(c)
+    {}
+
+    void Int2BvPreprocessor::reset() {
+        m_handled_vars.clear();
+        m_vars_bound_lemmas = z3::expr_vector(m_ctx);
+        m_const_out_of_bounds.clear();
+        m_func_app_out_of_bounds.clear();
+        m_literals.clear();
+    }
+
+    int Int2BvPreprocessor::num_of_conjuncts(const z3::expr& e) const {
+        if (e.is_and()) {
+            return e.num_args();
+        }
+        return 1;
+    }
+
+    int Int2BvPreprocessor::num_of_disjuncts(const z3::expr& e) const {
+        if (e.is_or()) {
+            return e.num_args();
+        }
+        return 1;
+    }
+
+    bool Int2BvPreprocessor::is_const_variable(const z3::expr& e) const {
+        // Check if the expression is a variable (0-arity application)
+        return e.is_const() && !e.is_numeral() && !e.is_bool();
+    }
+
+    z3::expr Int2BvPreprocessor::create_term_out_of_bounds_expr(const z3::expr& e) const {
+        Z3_decl_kind e_kind = e.decl().decl_kind();
+        z3::expr res(m_ctx);
+
+        if (m_is_signed) {
+            int64_t N = (int64_t)1 << (m_bv_size - 1);
+            switch (e_kind) {
+                case Z3_OP_ADD:
+                case Z3_OP_SUB:
+                case Z3_OP_MUL:
+                    res = !create_bounds_expr(e);
+                    break;
+                case Z3_OP_UMINUS:
+                    res = (e.arg(0) == m_ctx.int_val(-N));
+                    break;
+                case Z3_OP_DIV:
+                case Z3_OP_IDIV:
+                    res = ((e.arg(0) == m_ctx.int_val(-N)) && (e.arg(1) == m_ctx.int_val(-1)));
+                    break;
+                default:
+                    UNREACHABLE();
+            }
+        }
+        else {
+            int64_t N = (uint64_t)1 << m_bv_size;
+            switch (e_kind) {
+                case Z3_OP_ADD:
+                    res = (e > m_ctx.int_val(N - 1));
+                    break;
+                case Z3_OP_SUB:
+                    res = (e.arg(0) < e.arg(1));
+                    break;
+                case Z3_OP_MUL:
+                    res = (e > m_ctx.int_val(N - 1));
+                    break;
+                case Z3_OP_UMINUS:
+                    res = (e.arg(0) != m_ctx.int_val(0));
+                    break;
+                default:
+                    UNREACHABLE();
+            }
+        }
+        return res;
+    }
+
+    void Int2BvPreprocessor::handle_term(const z3::expr& e, z3::expr_vector& func_app_out_of_bounds) const {
+        Z3_decl_kind e_kind = e.decl().decl_kind();
+        if (m_is_signed) {
+            // Handle signed operations
+            if (e_kind == Z3_OP_ADD || e_kind == Z3_OP_SUB ||
+                e_kind == Z3_OP_MUL || e_kind == Z3_OP_UMINUS ||
+                e_kind == Z3_OP_DIV || e_kind == Z3_OP_IDIV) {
+                z3::expr out_of_bounds_expr = create_term_out_of_bounds_expr(e);
+                func_app_out_of_bounds.push_back(out_of_bounds_expr);
+            }
+        }
+        
+        if (e_kind == Z3_OP_ADD || e_kind == Z3_OP_SUB ||
+            e_kind == Z3_OP_MUL || e_kind == Z3_OP_UMINUS) {
+            // Handle unsigned operations
+            z3::expr out_of_bounds_expr = create_term_out_of_bounds_expr(e);
+            func_app_out_of_bounds.push_back(out_of_bounds_expr);
+        }
+    }
+    
+    z3::expr Int2BvPreprocessor::create_bounds_expr(const z3::expr& term) const {
+        if (m_is_signed) {
+            int64_t N = (int64_t)1 << (m_bv_size - 1);
+            return (term >= m_ctx.int_val(-N)) && (term <= m_ctx.int_val(N - 1));
+        }
+
+        int64_t N = (uint64_t)1 << m_bv_size;
+        return (term >= m_ctx.int_val(0)) && (term <= m_ctx.int_val(N - 1));
+    }
+
+    // TODO: Consider deleting this if unused
+    bool Int2BvPreprocessor::is_const_in_bounds(const z3::expr& const_e) const {
+        assert(const_e.is_numeral() && "Expected a constant expression");
+        if (m_is_signed) {
+            int64_t N = (int64_t)1 << (m_bv_size - 1);
+            return const_e.get_numeral_int() >= -N && const_e.get_numeral_int() <= N - 1;
+        }
+
+        int64_t N = (uint64_t)1 << m_bv_size;
+        return const_e.get_numeral_int() >= 0 && const_e.get_numeral_int() <= N - 1;
+    }
+
+    void Int2BvPreprocessor::populate_data_structures(const z3::expr& e) {
+        int n_conjuncts = num_of_conjuncts(e);
+
+        m_const_out_of_bounds.resize(n_conjuncts);
+        m_func_app_out_of_bounds.resize(n_conjuncts);
+        m_literals.resize(n_conjuncts);
+
+        for (int i = 0; i < n_conjuncts; ++i) {
+            z3::expr conjunct = (n_conjuncts == 1) ? e : e.arg(i);
+            int n_disjuncts = num_of_disjuncts(conjunct);
+
+            m_const_out_of_bounds[i].resize(n_disjuncts, false);
+            m_func_app_out_of_bounds[i].resize(n_disjuncts, z3::expr_vector(m_ctx));
+            m_literals[i].resize(n_disjuncts, z3::expr(m_ctx));
+
+            for (int j = 0; j < n_disjuncts; ++j) {
+                z3::expr literal = (n_disjuncts == 1) ? conjunct : conjunct.arg(j);
+                z3::expr_vector literal_func_app_out_of_bounds(m_ctx);
+                bool literal_const_out_of_bounds = false;
+
+                analyze_literal(literal, literal_const_out_of_bounds, literal_func_app_out_of_bounds);
+
+                m_literals[i][j] = literal;
+                m_const_out_of_bounds[i][j] = literal_const_out_of_bounds;
+                m_func_app_out_of_bounds[i][j] = literal_func_app_out_of_bounds;
+            }
+        }
+    }
+    
+    void Int2BvPreprocessor::analyze_literal(const z3::expr& literal, bool& const_out_of_bounds,
+                                             z3::expr_vector& func_app_out_of_bounds) {
+        if (literal.is_true() || literal.is_false()) {
+            return;
+        }
+        else if (literal.is_numeral()) {
+            bool in_bounds = is_const_in_bounds(literal);
+            const_out_of_bounds |= !in_bounds;
+        }
+        else if (is_const_variable(literal) && 
+                 m_handled_vars.find((Z3_ast)literal) == m_handled_vars.end()) {
+            // Add the variable to the cache if not already handled
+            m_handled_vars.insert((Z3_ast)literal);
+            z3::expr var_bound_expr = create_bounds_expr(literal);
+            m_vars_bound_lemmas.push_back(var_bound_expr);
+        }
+        else {
+            handle_term(literal, func_app_out_of_bounds);
+
+            for (unsigned i = 0; i < literal.num_args(); ++i) {
+                analyze_literal(literal.arg(i), const_out_of_bounds, func_app_out_of_bounds);
+            }
+        }
+    }
+
+    z3::expr Int2BvPreprocessor::create_SAT_out_of_bounds_expr(const z3::expr& e) const {
+        z3::expr bounds = z3::mk_and(m_vars_bound_lemmas);
+        z3::expr rest = m_ctx.bool_val(false);
+        int n_conjuncts = m_const_out_of_bounds.size();
+        
+        for (int i = 0; i < n_conjuncts; ++i) {
+            int n_disjuncts = m_const_out_of_bounds[i].size();
+            bool const_out_of_bounds = false;
+            for (int j = 0; j < n_disjuncts; ++j) {
+                if(!m_const_out_of_bounds[i][j]) {
+                    const_out_of_bounds = true;
+                    break;
+                }
+            }
+
+            // An optimization instead of creating a large expression
+            if (!const_out_of_bounds) {
+                continue;
+            }
+            else {
+                z3::expr conjuncts = m_ctx.bool_val(true);
+                for (int j = 0; j < n_disjuncts; ++j) {
+                    if (m_const_out_of_bounds[i][j]) {
+                        continue;
+                    }
+                    
+                    conjuncts = conjuncts && (!m_literals[i][j] || z3::mk_or(m_func_app_out_of_bounds[i][j]));
+                }
+                rest = rest || conjuncts;
+            }
+        }
+        z3::expr res(m_ctx);
+        res = e && bounds && rest;
+        return res.simplify();
+    }
+
+    z3::expr Int2BvPreprocessor::create_SAT_out_of_bounds(const z3::expr& e) {
+        populate_data_structures(e);
+        return create_SAT_out_of_bounds_expr(e);
+    }
+
+    z3::expr Int2BvPreprocessor::preprocess(const z3::expr& e) {
+        populate_data_structures(e);
+        z3::expr psi = create_SAT_out_of_bounds_expr(e);
+        // TODO: Continue logic ...
+        return psi;
+    }
+} // namespace multi_theory_horn
---
+++ src/Int2BvPreprocessor.h
@@ -0,0 +1,58 @@
+//------------------------------------------------------------------------------
+// Int2BvPreprocessor.h
+// A header file for the Int2BvPreprocessor class, which preprocesses
+// integer expressions to handle overflow and underflow
+//------------------------------------------------------------------------------
+
+#pragma once
+#include "utils.h"
+#include <z3++.h>
+#include <unordered_set>
+
+namespace multi_theory_horn {
+    class Int2BvPreprocessor {
+    private:
+        z3::context& m_ctx;
+        unsigned m_bv_size;
+        bool m_is_signed;
+
+        using ExprVectorMatrix = std::vector<std::vector<z3::expr_vector>>;
+        using LiteralMatrix = std::vector<std::vector<z3::expr>>;
+        using boolMatrix = std::vector<std::vector<bool>>;
+        using VarCache = std::unordered_set<Z3_ast, AstHash, AstEq>;
+
+        // Vars cache to not handle the same variable multiple times
+        VarCache m_handled_vars;
+
+        // Data structures to store info to build SATOutOfBounds and UNSATOutOfBounds
+        // expressions
+        z3::expr_vector m_vars_bound_lemmas;
+        boolMatrix m_const_out_of_bounds;
+        ExprVectorMatrix m_func_app_out_of_bounds;
+        LiteralMatrix m_literals;
+
+
+        // Returns the number of conjuncts in an expression
+        int num_of_conjuncts(const z3::expr& e) const;
+        int num_of_disjuncts(const z3::expr& e) const;
+
+        bool is_const_variable(const z3::expr& e) const;
+
+        z3::expr create_bounds_expr(const z3::expr& term) const;
+        bool is_const_in_bounds(const z3::expr& const_e) const;
+        z3::expr create_term_out_of_bounds_expr(const z3::expr& e) const;
+
+        void populate_data_structures(const z3::expr& e);
+        void handle_term(const z3::expr& term, z3::expr_vector& func_app_out_of_bounds) const;
+        void analyze_literal(const z3::expr& literal, bool& const_out_of_bounds, z3::expr_vector& func_app_out_of_bounds);
+
+        // The below functions assume the data structures are already populated
+        z3::expr create_SAT_out_of_bounds_expr(const z3::expr& e) const;
+    public:
+        explicit Int2BvPreprocessor(z3::context& c, unsigned bv_size, bool is_signed);
+        void reset();
+
+        z3::expr create_SAT_out_of_bounds(const z3::expr& e);
+        z3::expr preprocess(const z3::expr& e);
+    };
+} // namespace multi_theory_horn
