5750c8c7bf5fd91e054a3d78e24f32d0fec6ae3f
omerap <omer.r@campus.technion.ac.il>
2026-01-07 12:29:11 +0200

    Add cond-negate-concat benchmark and hints for testing

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -78,21 +78,25 @@ check_result max_inv(unsigned int size, bool is_multi) {
 
     // Rules
     // x > y, x - y >= z --> p(y,z,x,0)
+    // fragment: IA[size]
     expr rule1 = forall(x, y, z, implies(ugt(x, y) && uge(x - y, z), p(y, z, x, c.bv_val(0, size))));
     symbol name1 = c.str_symbol("rule1");
     fp.add_rule(rule1, name1);
 
     // p(y,z,a,i), i < z --> p(y,z,a-1,i+1)
+    // fragment: IA[size]
     expr rule2 = forall(vars, implies(p(y, z, a, i) && ult(i, z), p(y, z, a - c.bv_val(1, size), i + c.bv_val(1, size))));
     symbol name2 = c.str_symbol("rule2");
     fp.add_rule(rule2, name2);
 
     // p(y,z,a,i), !(i < z) --> q(y,a)
+    // fragment: IA[size]
     expr rule3 = forall(vars, implies(p(y, z, a, i) && !ult(i, z), q(y, a)));
     symbol name3 = c.str_symbol("rule3");
     fp.add_rule(rule3, name3);
 
     // q(y,a), !(a == max(a,y)) --> false
+    // fragment: BV
     expr_vector query_vars(c);
     query_vars.push_back(y);
     query_vars.push_back(a);
@@ -102,6 +106,7 @@ check_result max_inv(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
+        // interface constraints: q^{size} --> q
         MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
@@ -147,21 +152,25 @@ check_result opposite_signs(unsigned int size, bool is_multi) {
 
     // Rules
     // x > 0 --> p(x,0,0)
+    // fragment: IA[size]
     expr rule1 = forall(x, implies(x > 0, p(x, c.bv_val(0, size), c.bv_val(0, size))));     
     symbol name1 = c.str_symbol("rule1");
     fp.add_rule(rule1, name1);
 
     // p(x,a,b), a < x --> p(x,a + 1,b - 1)
+    // fragment: IA[size]
     expr rule2 = forall(vars, implies(p(x, a, b) && (a < x), p(x, a + c.bv_val(1, size), b - c.bv_val(1, size))));
     symbol name2 = c.str_symbol("rule2");
     fp.add_rule(rule2, name2);
 
     // p(x,a,b), !(a < x) --> q(a,b)
+    // fragment: IA[size]
     expr rule3 = forall(vars, implies(p(x, a, b) && !(a < x), q(a, b)));
     symbol name3 = c.str_symbol("rule3");
     fp.add_rule(rule3, name3);
 
     // q(a,b), !(a,b have opposite signs) --> false
+    // fragment: BV
     expr_vector query_vars(c);
     query_vars.push_back(a);
     query_vars.push_back(b);
@@ -171,6 +180,7 @@ check_result opposite_signs(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
+        // interface constraints: q^{size} --> q
         MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
@@ -216,22 +226,26 @@ check_result abs_ge(unsigned int size, bool is_multi) {
 
     // Rules
     // x != -2^(k-1), y == |x| --> p(x,y,0)
+    // fragment: BV
     expr min_val = shl(c.bv_val(1,size), c.bv_val(size - 1,size));
     expr rule1 = forall(x, y, implies((x != min_val) && (y == (c.bv_val(1,size) | ite(x >= 0, c.bv_val(0, size), c.bv_val(-1, size))) * x), p(x, y, c.bv_val(0, size))));     
     symbol name1 = c.str_symbol("rule1");
     fp.add_rule(rule1, name1);
 
     // p(x,y,i), i < y --> p(x,y,i + 1)
+    // fragment: IA[size]
     expr rule2 = forall(vars, implies(p(x, y, i) && (i < y), p(x, y, i + c.bv_val(1, size))));      
     symbol name2 = c.str_symbol("rule2");
     fp.add_rule(rule2, name2);
 
     // p(x,y,i), !(i < y) --> q(x,i)
+    // fragment: IA[size]
     expr rule3 = forall(vars, implies(p(x, y, i) && !(i < y), q(x, i)));      
     symbol name3 = c.str_symbol("rule3");
     fp.add_rule(rule3, name3);
 
     // q(x,i), !(x <= i) --> false
+    // fragment: IA[size]
     expr_vector query_vars(c);
     query_vars.push_back(x);
     query_vars.push_back(i);
@@ -241,6 +255,7 @@ check_result abs_ge(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
+        // interface constraints: p --> p^{size}
         MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
@@ -282,21 +297,25 @@ check_result cond_negate(unsigned int size, bool is_multi) {
 
     // Rules
     // x > y, y > 0 --> p(x,y,0)
+    // fragment: IA[size]
     expr rule1 = forall(x, y, implies(x > y && y > c.bv_val(0, size), p(x, y, c.bv_val(0, size))));     
     symbol name1 = c.str_symbol("rule1");
     fp.add_rule(rule1, name1);
 
     // p(x,y,i), i < y --> p(x,y,i + 2)
+    // fragment: IA[size]
     expr rule2 = forall(x, y, i, implies(p(x, y, i) && (i < y), p(x, y, i + 2)));      
     symbol name2 = c.str_symbol("rule2");
     fp.add_rule(rule2, name2);
 
     // p(x,y,i), !(i < y) --> q(x,i)
+    // fragment: IA[size]
     expr rule3 = forall(x, y, i, implies(p(x, y, i) && !(i < y), q(x, i)));      
     symbol name3 = c.str_symbol("rule3");
     fp.add_rule(rule3, name3);
 
     // q(x,i), b == ite(i <= x,1,0), !((x ^ (-b)) + b == -x) --> false
+    // fragment: BV
     expr_vector query_vars(c);
     query_vars.push_back(x);
     query_vars.push_back(i);
@@ -307,6 +326,7 @@ check_result cond_negate(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
+        // interface constraints: q^{size} --> q
         MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
@@ -352,31 +372,37 @@ check_result swap(unsigned int size, bool is_multi) {
 
     // Rules
     // ugt(x,y), ugt(y,0) --> p(x,y,0,0)
+    // fragment: IA[size]
     expr rule1 = forall(x, y, implies(ugt(x, y) && ugt(y, 0), p(x, y, c.bv_val(0, size), c.bv_val(0, size))));
     symbol name1 = c.str_symbol("rule1");
     fp.add_rule(rule1, name1);
 
     // p(x,y,a,b), ult(b,y) --> p(x,y,a+1,b+1) 
+    // fragment: IA[size]
     expr rule2 = forall(x, y, a, b, implies(p(x, y, a, b) && ult(b, y), p(x, y, a + c.bv_val(1, size), b + c.bv_val(1, size))));
     symbol name2 = c.str_symbol("rule2");
     fp.add_rule(rule2, name2);
 
     // p(x,y,a,b), !ult(b,y) --> q(x,a,b)
+    // fragment: IA[size]
     expr rule3 = forall(x, y, a, b, implies(p(x, y, a, b) && !ult(b, y), q(x, a, b)));
     symbol name3 = c.str_symbol("rule3");
     fp.add_rule(rule3, name3);
 
     // q(x,a,b), ult(a,x) --> q(x,a+1,b)
+    // fragment: IA[size]
     expr rule4 = forall(x, a, b, implies(q(x, a, b) && ult(a, x), q(x, a + c.bv_val(1, size), b)));
     symbol name4 = c.str_symbol("rule4");
     fp.add_rule(rule4, name4);
 
     // q(x,a,b), !ult(a,x) --> r(a,b)
+    // fragment: IA[size]
     expr rule5 = forall(x, a, b, implies(q(x, a, b) && !ult(a, x), r(a, b)));
     symbol name5 = c.str_symbol("rule5");
     fp.add_rule(rule5, name5);
 
     // r(a,b), a1 = a ^ b, b1 = b ^ a1, !ult(a1^b1, b1) --> false 
+    // fragment: BV
     expr_vector query_vars(c);
     query_vars.push_back(a);
     query_vars.push_back(b);
@@ -388,6 +414,7 @@ check_result swap(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
+        // interface constraints: r^{size} --> r
         MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
@@ -441,21 +468,25 @@ check_result opposite_signs_concat(unsigned int size, bool is_multi) {
     // Rules
 
     // x > 0 --> p(x,0,0)
+    // fragment: IA[size]
     expr rule1 = forall(x, implies(x > 0, p(x, c.bv_val(0, size), c.bv_val(0, size))));     
     symbol name1 = c.str_symbol("rule1");
     fp.add_rule(rule1, name1);
 
     // p(x,a,b), a < x --> p(x,a + 1,b - 1)
+    // fragment: IA[size]
     expr rule2 = forall(x, a, b, implies(p(x, a, b) && (a < x), p(x, a + c.bv_val(1, size), b - c.bv_val(1, size))));
     symbol name2 = c.str_symbol("rule2");
     fp.add_rule(rule2, name2);
 
     // p(x,a,b), !(a < x) --> q(a,b)
+    // fragment: IA[size]
     expr rule3 = forall(x, a, b, implies(p(x, a, b) && !(a < x), q(a, b)));
     symbol name3 = c.str_symbol("rule3");
     fp.add_rule(rule3, name3);
 
     // q(a,b), a2=concat(a,d), b2=concat(b,d), i=concat(0,d) --> r(a2,b2,i)
+    // fragment: BV
     expr_vector vars(c);
     vars.push_back(a);
     vars.push_back(b);
@@ -469,16 +500,19 @@ check_result opposite_signs_concat(unsigned int size, bool is_multi) {
     fp.add_rule(rule4, name4);
 
     // r(a2,b2,i), i > 0 --> r(a2 - 1, b2 - 1, i - 1)
+    // fragment: IA[2*size]
     expr rule5 = forall(a2, b2, i, implies(r(a2,b2,i) && i > 0, r(a2 - c.bv_val(1, d_size), b2 - c.bv_val(1, d_size), i - c.bv_val(1, d_size))));
     symbol name5 = c.str_symbol("rule5");
     fp.add_rule(rule5, name5);
 
     // r(a2,b2,i), !(i > 0) --> s(a2, b2)
+    // fragment: IA[2*size]
     expr rule6 = forall(a2, b2, i, implies(r(a2,b2,i) && !(i > 0), s(a2, b2)));
     symbol name6 = c.str_symbol("rule6");
     fp.add_rule(rule6, name6);
 
     // s(a2,b2), !(a,b have opposite signs) --> false
+    // fragment: BV
     expr_vector query_vars(c);
     query_vars.push_back(a2);
     query_vars.push_back(b2);
@@ -488,6 +522,7 @@ check_result opposite_signs_concat(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
+        // interface constraints: q^{size} --> q, r --> r^{2*size}, s^{2*size} --> s
         MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
@@ -499,6 +534,116 @@ check_result opposite_signs_concat(unsigned int size, bool is_multi) {
     return result;
 }
 // ================== [opposite-signs-concat] ==================
+
+// ==================== [cond-negate-concat] ===================
+// signed benchmark
+check_result cond_negate_concat(unsigned int size, bool is_multi) {
+    context c;
+    fixedpoint fp(c);
+
+    unsigned int d_size = size * 2;
+
+    // Declare sorts
+    sort bv_sort = c.bv_sort(size);
+    sort bv_d_sort = c.bv_sort(d_size);
+    sort bool_sort = c.bool_sort();
+
+    // Declare relations
+    func_decl p = function("p", bv_sort, bv_sort, bv_sort, bool_sort);
+    func_decl q = function("q", bv_sort, bv_sort, bool_sort);
+    func_decl r = function("r", bv_d_sort, bv_d_sort, bv_d_sort, bool_sort);
+    func_decl s = function("s", bv_d_sort, bv_d_sort, bool_sort);
+
+    // Register them with the fixedpoint engine (required)
+    fp.register_relation(p);
+    fp.register_relation(q);
+    fp.register_relation(r);
+    fp.register_relation(s);
+
+    params param = get_bv_params(c);
+    fp.set(param);
+
+    // Variables
+    expr x = c.bv_const("x", size);
+    expr y = c.bv_const("y", size);
+    expr i = c.bv_const("i", size);
+    expr d = c.bv_const("d", size);
+
+    expr x2 = c.bv_const("x2", d_size);
+    expr i2 = c.bv_const("i2", d_size);
+    expr j = c.bv_const("j", d_size);
+    expr b = c.bv_const("b", d_size);
+
+    // Rules
+    
+    // x > y, y > 0 --> p(x,y,0)
+    // fragment: IA[size]
+    expr rule1 = forall(x, y, implies(x > y && y > c.bv_val(0, size), p(x, y, c.bv_val(0, size))));     
+    symbol name1 = c.str_symbol("rule1");
+    fp.add_rule(rule1, name1);
+
+    // p(x,y,i), i < y --> p(x,y,i + 2)
+    // fragment: IA[size]
+    expr rule2 = forall(x, y, i, implies(p(x, y, i) && (i < y), p(x, y, i + 2)));      
+    symbol name2 = c.str_symbol("rule2");
+    fp.add_rule(rule2, name2);
+
+    // p(x,y,i), !(i < y) --> q(x,i)
+    // fragment: IA[size]
+    expr rule3 = forall(x, y, i, implies(p(x, y, i) && !(i < y), q(x, i)));      
+    symbol name3 = c.str_symbol("rule3");
+    fp.add_rule(rule3, name3);
+
+    // q(x,i), x2=concat(x,d), i2=concat(i,d), j=concat(0,d) --> r(x2,i2,j)
+    // fragment: BV
+    expr_vector vars(c);
+    vars.push_back(x);
+    vars.push_back(i);
+    vars.push_back(d);
+    vars.push_back(x2);
+    vars.push_back(i2);
+    vars.push_back(j);
+    expr zero = c.bv_val(0, size);
+    expr rule4 = forall(vars, implies(q(x,i) && (x2 == concat(x,d)) && (i2 == concat(i,d)) && (j == concat(zero,d)), r(x2,i2,j)));
+    symbol name4 = c.str_symbol("rule4");
+    fp.add_rule(rule4, name4);
+
+    // r(x2,i2,j), j > 0 --> r(x2 - 1, i2 - 1, j - 1)
+    // fragment: IA[2*size]
+    expr rule5 = forall(x2, i2, j, implies(r(x2,i2,j) && j > 0, r(x2 - c.bv_val(1, d_size), i2 - c.bv_val(1, d_size), j - c.bv_val(1, d_size))));
+    symbol name5 = c.str_symbol("rule5");
+    fp.add_rule(rule5, name5);
+
+    // r(x2,i2,j), !(j > 0) --> s(x2, i2)
+    // fragment: IA[2*size]
+    expr rule6 = forall(x2, i2, j, implies(r(x2,i2,j) && !(j > 0), s(x2, i2)));
+    symbol name6 = c.str_symbol("rule6");
+    fp.add_rule(rule6, name6);
+
+    // s(x2,i2), b == ite(i2 <= x2,1,0), !((x2 ^ (-b)) + b == -x2) --> false
+    // fragment: BV
+    expr_vector query_vars(c);
+    query_vars.push_back(x2);
+    query_vars.push_back(i2);
+    query_vars.push_back(b);
+    expr query_pred = s(x2, i2);
+    expr bad_phi = (b == ite(i2 <= x2, c.bv_val(1, d_size), c.bv_val(0, d_size))) && !(((x2 ^ (-b)) + b) == -x2);
+    
+    check_result result = check_result::unknown;
+
+    if (is_multi) {
+        // interface constraints: q^{size} --> q, r --> r^{2*size}, s^{2*size} --> s
+        MT_fixedpoint mtfp(c);
+        mtfp.from_solver(fp);
+        result = mtfp.query(query_vars, query_pred, bad_phi); 
+    } else { // bv only
+        expr query = exists(query_vars, query_pred && bad_phi);
+        result = fp.query(query);
+    }
+
+    return result;
+}
+// ==================== [cond-negate-concat] ===================
 
 // ======================= MAIN LOGIC =======================
 
@@ -552,6 +697,7 @@ static int run_benchmarks_cli(int argc, char** argv) {
         {"cond-negate",                 {cond_negate,                   true}},
         {"swap",                        {swap,                          true}},
         {"opposite-signs-concat",       {opposite_signs_concat,         true}},
+        {"cond-negate-concat",          {cond_negate_concat,            true}},
     };
 
     std::string bench = "";
