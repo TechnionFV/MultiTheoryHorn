3274bd8ccc005341d7a6b16e8eca550f0886a9de
omerap <omer.r@campus.technion.ac.il>
2026-01-07 11:11:49 +0200

    Add opposite-signs-concat benchmark with BV variables of two different sizes

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -102,7 +102,7 @@ check_result max_inv(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
-        MT_fixedpoint mtfp(c, /* is_signed= */ false, size, /* int2bv_preprocess */ !gno_int2bv_preprocess);
+        MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
     } else { // bv only
@@ -171,7 +171,7 @@ check_result opposite_signs(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
-        MT_fixedpoint mtfp(c, /* is_signed= */ true, size, /* int2bv_preprocess */ !gno_int2bv_preprocess);
+        MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
     } else { // bv only
@@ -241,7 +241,7 @@ check_result abs_ge(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
-        MT_fixedpoint mtfp(c, /* is_signed= */ true, size, /* int2bv_preprocess */ !gno_int2bv_preprocess);
+        MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
     } else { // bv only
@@ -307,7 +307,7 @@ check_result cond_negate(unsigned int size, bool is_multi) {
     check_result result = check_result::unknown;
 
     if (is_multi) {
-        MT_fixedpoint mtfp(c, /* is_signed= */ true, size, /* int2bv_preprocess */ !gno_int2bv_preprocess);
+        MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
     } else { // bv only
@@ -383,13 +383,12 @@ check_result swap(unsigned int size, bool is_multi) {
     query_vars.push_back(a1);
     query_vars.push_back(b1);
     expr query_pred = r(a, b);
-    //expr bad_phi = ((a1 == a ^ b) && (b1 == b ^ a1) && !ult(a1 ^ b1, b1));
     expr bad_phi = (a1 == (a ^ b)) && (b1 == (b ^ a1)) && !ult((a1 ^ b1), b1);
 
     check_result result = check_result::unknown;
 
     if (is_multi) {
-        MT_fixedpoint mtfp(c, /* is_signed= */ false, size, /* int2bv_preprocess */ !gno_int2bv_preprocess);
+        MT_fixedpoint mtfp(c);
         mtfp.from_solver(fp);
         result = mtfp.query(query_vars, query_pred, bad_phi); 
     } else { // bv only
@@ -400,6 +399,106 @@ check_result swap(unsigned int size, bool is_multi) {
     return result;
 }
 // =========================== [swap] ==========================
+
+// ================== [opposite-signs-concat] ==================
+// signed benchmark
+check_result opposite_signs_concat(unsigned int size, bool is_multi) {
+    context c;
+    fixedpoint fp(c);
+
+    unsigned int d_size = size * 2;
+
+    // Declare sorts
+    sort bv_sort = c.bv_sort(size);
+    sort bv_d_sort = c.bv_sort(d_size);
+    sort bool_sort = c.bool_sort();
+
+    // Declare relations
+    func_decl p = function("p", bv_sort, bv_sort, bv_sort, bool_sort);
+    func_decl q = function("q", bv_sort, bv_sort, bool_sort);
+    func_decl r = function("r", bv_d_sort, bv_d_sort, bv_d_sort, bool_sort);
+    func_decl s = function("s", bv_d_sort, bv_d_sort, bool_sort);
+
+    // Register them with the fixedpoint engine (required)
+    fp.register_relation(p);
+    fp.register_relation(q);
+    fp.register_relation(r);
+    fp.register_relation(s);
+
+    params param = get_bv_params(c);
+    fp.set(param);
+
+    // Variables
+    expr x = c.bv_const("x", size);
+    expr a = c.bv_const("a", size);
+    expr b = c.bv_const("b", size);
+    expr d = c.bv_const("d", size);
+    
+    expr a2 = c.bv_const("a2", d_size);
+    expr b2 = c.bv_const("b2", d_size);
+    expr i = c.bv_const("x", d_size);
+
+    // Rules
+
+    // x > 0 --> p(x,0,0)
+    expr rule1 = forall(x, implies(x > 0, p(x, c.bv_val(0, size), c.bv_val(0, size))));     
+    symbol name1 = c.str_symbol("rule1");
+    fp.add_rule(rule1, name1);
+
+    // p(x,a,b), a < x --> p(x,a + 1,b - 1)
+    expr rule2 = forall(x, a, b, implies(p(x, a, b) && (a < x), p(x, a + c.bv_val(1, size), b - c.bv_val(1, size))));
+    symbol name2 = c.str_symbol("rule2");
+    fp.add_rule(rule2, name2);
+
+    // p(x,a,b), !(a < x) --> q(a,b)
+    expr rule3 = forall(x, a, b, implies(p(x, a, b) && !(a < x), q(a, b)));
+    symbol name3 = c.str_symbol("rule3");
+    fp.add_rule(rule3, name3);
+
+    // q(a,b), a2=concat(a,d), b2=concat(b,d), i=concat(0,d) --> r(a2,b2,i)
+    expr_vector vars(c);
+    vars.push_back(a);
+    vars.push_back(b);
+    vars.push_back(d);
+    vars.push_back(a2);
+    vars.push_back(b2);
+    vars.push_back(i);
+    expr zero = c.bv_val(0, size);
+    expr rule4 = forall(vars, implies(q(a,b) && (a2 == concat(a,d)) && (b2 == concat(b,d)) && (i == concat(zero,d)), r(a2,b2,i)));
+    symbol name4 = c.str_symbol("rule4");
+    fp.add_rule(rule4, name4);
+
+    // r(a2,b2,i), i > 0 --> r(a2 - 1, b2 - 1, i - 1)
+    expr rule5 = forall(a2, b2, i, implies(r(a2,b2,i) && i > 0, r(a2 - c.bv_val(1, d_size), b2 - c.bv_val(1, d_size), i - c.bv_val(1, d_size))));
+    symbol name5 = c.str_symbol("rule5");
+    fp.add_rule(rule5, name5);
+
+    // r(a2,b2,i), !(i > 0) --> s(a2, b2)
+    expr rule6 = forall(a2, b2, i, implies(r(a2,b2,i) && !(i > 0), s(a2, b2)));
+    symbol name6 = c.str_symbol("rule6");
+    fp.add_rule(rule6, name6);
+
+    // s(a2,b2), !(a,b have opposite signs) --> false
+    expr_vector query_vars(c);
+    query_vars.push_back(a2);
+    query_vars.push_back(b2);
+    expr query_pred = s(a2, b2);
+    expr bad_phi = !((a2 ^ b2) < 0);
+
+    check_result result = check_result::unknown;
+
+    if (is_multi) {
+        MT_fixedpoint mtfp(c);
+        mtfp.from_solver(fp);
+        result = mtfp.query(query_vars, query_pred, bad_phi); 
+    } else { // bv only
+        expr query = exists(query_vars, query_pred && bad_phi);
+        result = fp.query(query);
+    }
+
+    return result;
+}
+// ================== [opposite-signs-concat] ==================
 
 // ======================= MAIN LOGIC =======================
 
@@ -452,6 +551,7 @@ static int run_benchmarks_cli(int argc, char** argv) {
         {"abs-ge",                      {abs_ge,                        true}},
         {"cond-negate",                 {cond_negate,                   true}},
         {"swap",                        {swap,                          true}},
+        {"opposite-signs-concat",       {opposite_signs_concat,         true}},
     };
 
     std::string bench = "";
