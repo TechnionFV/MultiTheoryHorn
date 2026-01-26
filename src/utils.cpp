370b395f275c38571d5c0ea0c13cce77f9cf8492
Thomas Hashem <hashemthomas1@gmail.com>
2026-01-27 00:29:18 +0200

    [AMTH] Add get_bv_app_based_on_decl utility
    The function inspects the declaration kind given as parameter
    and returns an application expression of this declaration kind
    with the given arguments.

--- src/utils.cpp
+++ src/utils.cpp
@@ -56,4 +56,121 @@ namespace utils {
     uint64_t get_unsigned_bv_lower_bound(unsigned bv_size) {
         return 0;
     }
+
+    z3::expr get_bv_app_based_on_decl(z3::context& ctx,
+                                      const Z3_decl_kind& decl_kind,
+                                      const z3::expr_vector& args) {
+        switch (decl_kind) {
+            // ------------------------------------------------------------------
+            // Basic
+            // ------------------------------------------------------------------
+            case Z3_OP_EQ:
+                return args[0] == args[1];
+            case Z3_OP_DISTINCT:
+                return z3::distinct(args);
+            case Z3_OP_ITE:
+                return z3::ite(args[0], args[1], args[2]);
+            case Z3_OP_AND:
+                return z3::mk_and(args);
+            case Z3_OP_OR:
+                return z3::mk_or(args);
+            case Z3_OP_XOR:
+                return z3::mk_xor(args);
+            case Z3_OP_NOT:
+                return !args[0];
+            case Z3_OP_IMPLIES:
+                return z3::implies(args[0], args[1]);
+
+            // ------------------------------------------------------------------
+            // Arithmetic
+            // ------------------------------------------------------------------
+            case Z3_OP_BNEG:
+                return -args[0];
+            case Z3_OP_BADD:
+                return args[0] + args[1];
+            case Z3_OP_BSUB:
+                return args[0] - args[1];
+            case Z3_OP_BMUL:
+                return args[0] * args[1];
+
+            // ------------------------------------------------------------------
+            // Division and Remainder
+            // ------------------------------------------------------------------
+            case Z3_OP_BSDIV:
+            case Z3_OP_BSDIV_I:
+                return args[0] / args[1]; // operator/ is signed division in Z3 C++
+
+            case Z3_OP_BUDIV:
+            case Z3_OP_BUDIV_I:
+                return z3::udiv(args[0], args[1]);
+
+            case Z3_OP_BSREM:
+            case Z3_OP_BSREM_I:
+                return z3::srem(args[0], args[1]);
+
+            case Z3_OP_BUREM:
+            case Z3_OP_BUREM_I:
+                return z3::urem(args[0], args[1]);
+
+            case Z3_OP_BSMOD:
+            case Z3_OP_BSMOD_I:
+                return z3::smod(args[0], args[1]);
+
+            // ------------------------------------------------------------------
+            // Bitwise Logic
+            // ------------------------------------------------------------------
+            case Z3_OP_BNOT:
+                return ~args[0];
+            case Z3_OP_BAND:
+                return args[0] & args[1];
+            case Z3_OP_BOR:
+                return args[0] | args[1];
+            case Z3_OP_BXOR:
+                return args[0] ^ args[1];
+            case Z3_OP_BNAND:
+                return ~(args[0] & args[1]);
+            case Z3_OP_BNOR:
+                return ~(args[0] | args[1]);
+            case Z3_OP_BXNOR:
+                return ~(args[0] ^ args[1]);
+
+            // ------------------------------------------------------------------
+            // Structural
+            // ------------------------------------------------------------------
+            case Z3_OP_CONCAT:
+                return z3::concat(args[0], args[1]);
+
+            // ------------------------------------------------------------------
+            // Shifts
+            // ------------------------------------------------------------------
+            case Z3_OP_BSHL:
+                return z3::shl(args[0], args[1]);
+            case Z3_OP_BLSHR:
+                return z3::lshr(args[0], args[1]);
+            case Z3_OP_BASHR:
+                return z3::ashr(args[0], args[1]);
+
+            // ------------------------------------------------------------------
+            // Comparisons
+            // ------------------------------------------------------------------
+            case Z3_OP_ULEQ:
+                return args[0] <= args[1];
+            case Z3_OP_SLEQ:
+                return args[0] <= args[1];
+            case Z3_OP_UGEQ:
+                return args[0] >= args[1];
+            case Z3_OP_SGEQ:
+                return args[0] >= args[1];
+            case Z3_OP_ULT:
+                return args[0] < args[1];
+            case Z3_OP_SLT:
+                return args[0] < args[1];
+            case Z3_OP_UGT:
+                return args[0] > args[1];
+            case Z3_OP_SGT:
+                return args[0] > args[1];
+            default:
+                assert(false && "Unsupported decl kind in get_app_based_on_decl");
+        }
+    }
 } // namespace utils
